0cadede157c89197815755f7ad3efa0b8b1035dd
ABDULMALEK TAHA <ai240239@student.uthm.edu.my>
2025-10-26 22:34:11 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // -- [أسطر إظهار الأخطاء] --
-ini_set('display_errors', 1);
+ini_set('display_errors',0);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // -- [نهاية الأسطر المضافة] --
@@ -49,4 +49,5 @@ try {
 }
 
 ?>
+
 
